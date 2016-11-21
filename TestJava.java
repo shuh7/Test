f13ecb996d5019bfa921d2816bcbf7cdb4a28c32
shuh7 <shu.h7.develop@gmail.com>
2016-11-21 23:15:18 +0900

    Create TestJava.java

---
+++ TestJava.java
@@ -0,0 +1,9 @@
+//===================================================================
+//For the test.
+//===================================================================
+
+public class TestJava {
+  public static void main(String[] args) {
+    System.out.println("Hello, GitHub!!");
+  }
+}
